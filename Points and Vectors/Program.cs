397e1cbd8dd638a19b9eaff7e18d591eac4942de
Jahvier Hernandez <jahvierhernandez11@gmail.com>
2019-10-01 12:21:32 -0700

    Update

---
+++ Points and Vectors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Points_and_Vectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //float V4 = ;
+
+            //vector3 = vector3 * 2;
+            Vector2 vector2 = new Vector2(2, 7);
+            Vector2 vector4 = new Vector2(5, 6);
+            Vector3 vector3 = new Vector3(3, 8, 4);
+            Console.ReadKey();
+            
+        }
+            
+    }
+}
---
+++ Points and Vectors/Vectors.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Points_and_Vectors
+{
+    
+    struct Vector2
+    {
+        float x, y;
+        public Vector2(float _x, float _y)
+        {
+            x = _x;
+            y = _y;
+        }
+        public static Vector2 operator +(Vector2 lhs, Vector2 rhs)
+        {
+            return new Vector2(lhs.x + rhs.x, lhs.y + rhs.y);
+        }
+        public static Vector2 operator -(Vector2 lhs, Vector2 rhs)
+        {
+            return new Vector2(lhs.x - rhs.x, lhs.y - rhs.y);
+        }
+    }
+    struct Vector3
+    {
+        float x, y, z;
+        public Vector3(float _x, float _y, float _z)
+        {
+            x = _x;
+            y = _y;
+            z = _z;
+        }
+        public static Vector3 operator +(Vector3 lhs, Vector3 rhs)
+        {
+            return new Vector3(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+        }
+
+        public static Vector3 operator -(Vector3 lhs, Vector3 rhs)
+        {
+            return new Vector3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
+        }
+
+        public static Vector3 operator *(Vector3 lhs, float rhs)
+        {
+            return new Vector3(lhs.x * rhs, lhs.y * rhs, lhs.z * rhs);
+        }
+
+        public static Vector3 operator /(Vector3 lhs, float rhs)
+        {
+            return new Vector3(lhs.x / rhs, lhs.y / rhs, lhs.z / rhs);
+        }
+
+        //public static Vector3 operator +(Vector3 lhs, Vector3 rhs)
+        //{
+        //    return new Vector3(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+        //}
+
+        //public static Vector3 operator -(Vector3 lhs, Vector3 rhs)
+        //{
+        //    return new Vector3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
+        //}
+
+        
+    }}
